5034344628e076f49de574b2108b86e5bc3ba58e
Shelby Kelly <shelby.kelly@colorado.edu>
2014-10-02 15:43:31 -0600

    header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-   root 'pages#home'
+   root "pages#home"
+   get "login" => "sessions#new", as: :login
+   post "/login" => "sessions#create"
+   get "logout" => "sessions#destroy", as: :logout
   resources :artists
   resources :venues
 
